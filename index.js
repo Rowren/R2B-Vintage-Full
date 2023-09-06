0057eef8234464c85d81f6efbbe1f76693f4e722
rowren <bankgtgt@gmail.com>
2023-09-06 21:49:38 +0700

    commit-message

--- index.js
+++ index.js
@@ -1,25 +1,115 @@
 var product = [{
     id: 1,
-    img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
-    name: 'Nike',
-    price: 7000,
-    description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-    type: 'shoe'
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/372803899_268058562713108_1591103496286940829_n.jpg?stp=dst-jpg_s206x206&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeHcmg-Cv2ocJaJhEBbI_ceNB9mq_uXPScUH2ar-5c9Jxes9RV3Sc039ZbktVEA8e61TUbw2SU-GtggMSPXC4wkz&_nc_ohc=V2mMHDRnWqoAX95yZ_Z&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdT9n0fj0uGsznzB7o93nkkKBpzLbFmS50FQDdC52sfaBQ&oe=651D4443',
+    name: 'Wave fronnt Surf Shop',
+    price: 1500,
+    description: 'Size:M อก20ยาว27',
+    type: 'shirt'
 }, {
     id: 2,
-    img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
-    name: 'Adidas shirt',
-    price: 1500,
-    description: 'Adidas shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/371170757_608427524810620_2621678149661721372_n.jpg?stp=dst-jpg_s206x206&_nc_cat=104&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeG7CSOEqrT3SQX7VZMhLDUGQf-TJXaC7b9B_5MldoLtv8tGTMetEDje2bKIIpXtE8lBNPfobCrmpNRwJ6RW8aQX&_nc_ohc=TSWpj4svlzgAX8SrFc6&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdT4QJPV37oKTH3g1g57r1CErM9PiaxHMAM3j3W6UtJjvg&oe=651D5E2D',
+    name: 'The Beatles',
+    price: 2500,
+    description: 'Size:Mอก20ยาว28',
     type: 'shirt'
 }, {
     id: 3,
-    img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    name: 'Adidas shoe',
-    price: 45000,
-    description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/372410836_677888737558300_7538087500894888286_n.jpg?stp=dst-jpg_s206x206&_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeGDdDJN5y8S27zCjleuCFGlf_ARrHaNr2J_8BGsdo2vYh0IkF691OudNE5Mo8TDhtKYThme-R6rut_Ct2NhC6xY&_nc_ohc=jS7iXoa2NeQAX-dcJWF&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQuJ-wDj4OOk8xWPm4TdQjmzc8KH9PLqnBiJsG-eYuU3w&oe=651D294D',
+    name: 'Knotfest',
+    price: 1700,
+    description: 'Size:Mอก19ยาว27',
+    type: 'shirt'
+}, {
+    id: 4,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/370314336_7299148590111495_8790499743750658527_n.jpg?stp=dst-jpg_s206x206&_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeGyBJlFK4yEhfQ_QgrSPw3ZK04_bhI4ZHUrTj9uEjhkdRxekQWT_IV1uL75YVNaUMO0B_Ez7gPpF1U-B58-JM_D&_nc_ohc=Eb9Xr_-GK20AX8JjgOg&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRLoOCTpnRz0bGvREAxZZCpWLC2K6n8yZ8uRU6fotr0Hw&oe=651D37D8',
+    name: 'The Lightning Thief',
+    price: 3000,
+    description: 'Size:Mอก20ยาว27',
+    type: 'shirt'
+}, {
+    id: 5,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/370320504_680685373915630_7222314244294313959_n.jpg?stp=dst-jpg_s206x206&_nc_cat=107&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeFMzSFBwUOwUI_kF_SVmB9mjPBdPOjEi7mM8F086MSLuUU4vqWPQ94Do4R0G48xaxWwHcf9d5QwQ3XwWAJFrY4A&_nc_ohc=VLPV8O2-lEQAX8D5JJl&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRIrXhJJqDczFQ4kyerWXQUuRftpPPk54rbKNS5AK7EzA&oe=651D3B54',
+    name: 'TooL เสื้อวง ',
+    price: 5500,
+    description: 'Size:M-L อก20-21ยาย27',
+    type: 'shirt'
+}, {
+    id: 6,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/373067926_1429028817886475_5621048500759562800_n.jpg?stp=dst-jpg_s206x206&_nc_cat=106&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeG3wcn7LuL_0NXVs_ApRUPCDYdQhVVmMNsNh1CFVWYw276BpTDtbujZuyfYy-g0H21SxB9ICi9rowADoKzzKw1J&_nc_ohc=wNd9VU-DL4UAX9quNKE&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdT2PSHgbqXEI9x6JCZQ8_HhiT5u3_2Llc_M_OrysdMLBg&oe=651D3EC9',
+    name: 'Sons of anarchy',
+    price: 1400,
+    description: 'Size:Mอก19.5ยาว28',
+    type: 'shirt'
+}, {
+    id: 7,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/373423018_664222018990294_58497931268549763_n.jpg?stp=dst-jpg_p206x206&_nc_cat=108&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeFlgf36ZMqQg98I3m-wzuIVzv5hrYgcYO_O_mGtiBxg72KUkOl1R5Ojg1YGTh8bKmWN3kcogUitgxHpUTiYlORp&_nc_ohc=mF836YrmTt0AX_UW31u&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQWDpTf091PJqwkgtGuUUiDF0d86wH-Z142rotrIM9K-w&oe=651D2E3F',
+    name: 'Dickie ขาต่อ5กระเป๋า สีดำ',
+    price: 1500,
+    description: 'เอววัดจริง32  ปลายขา10 ความยาว45 แก้ไซร์สภาพดี เนื้อผ้าดี  ผ้าแข็งใหม่ สีดำสนิท ',
+    type: 'trousers'
+}, {
+    id: 8,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/372555248_956606472065769_142588091572612131_n.jpg?stp=dst-jpg_p206x206&_nc_cat=109&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeEFLMZ9Kl_UkibRG8n641-rUBFWz_zOIwlQEVbP_M4jCbS7HpgfTIMJpf16D7TDKnKtRc6S8PSyl3FsrQGdxka6&_nc_ohc=x7eQIpino-AAX8XjGGp&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQRnj6n_yykqQTRVMgeeYcksy0HEQGO8EjnVD2YUojZDg&oe=651D53B5',
+    name: 'Dickie ขาต่อ5กระเป๋า สีดำ',
+    price: 1500,
+    description: 'เอววัดจริง34”   ปลายขา10"    ความยาว43" สภาพดี เนื้อผ้าดี  ผ้าแข็งใหม่ สีดำสนิท  ',
+    type: 'trousers'
+}, {
+    id: 9,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/373036023_2579903095495010_174334015714559774_n.jpg?stp=dst-jpg_p206x206&_nc_cat=107&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeF9s-RIEpDaLzKp8wd-09P3Pu4Lvv5zfA8-7gu-_nN8D59xmyC3sGqHO4uQBM3OdXhgS2sB8LocnoGSLYRHpHmb&_nc_ohc=rCkqJHbnxB0AX-pTMXw&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQcPGAMhb1l1lBnqM_FPzkHMPYNNNkeVpClI-y6OlN_ZQ&oe=651D3B6E',
+    name: 'Dickie ขาต่อ5กระเป๋า สีกรม',
+    price: 1050,
+    description: 'เอววัดจริง36”   ปลายขา10”    ความยาว45”    สภาพดี เนื้อผ้าดี  ผ้าแข็งใหม่ สีกรมสนิท  ',
+    type: 'trousers'
+}, {
+    id: 10,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/373036023_2579903095495010_174334015714559774_n.jpg?stp=dst-jpg_p206x206&_nc_cat=107&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeF9s-RIEpDaLzKp8wd-09P3Pu4Lvv5zfA8-7gu-_nN8D59xmyC3sGqHO4uQBM3OdXhgS2sB8LocnoGSLYRHpHmb&_nc_ohc=rCkqJHbnxB0AX-pTMXw&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQcPGAMhb1l1lBnqM_FPzkHMPYNNNkeVpClI-y6OlN_ZQ&oe=651D3B6E',
+    name: 'Dickie ขาต่อ5กระเป๋า สีกรม',
+    price: 1250,
+    description: 'เอววัดจริง30”    ปลายขา10”   ความยาว43”    สภาพดี เนื้อผ้าดี  ผ้าแข็งใหม่ สีกรมสนิท  ',
+    type: 'trousers'
+}, {
+    id: 11,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/373006367_121194387742579_8450634508520797223_n.jpg?stp=dst-jpg_p206x206&_nc_cat=104&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeG9diVzNtuMfZazYuSg-7-cSZMNcFgOgalJkw1wWA6BqZJb5wEpKUOje0YnMLgB70PUPsz9VH6y5d2CFeITAonk&_nc_ohc=7LhYfFbeHgMAX8SLIqZ&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRJgbci3Y8pBfap3VV70zejBh5o1FDPkZJIea4rmt6Vbg&oe=651D4EF9',
+    name: 'Dickie ขาต่อ5กระเป๋า สีเทา',
+    price: 850,
+    description: 'เอววัดจริง38”    ปลายขา10”    ความยาว46”    สภาพดี เนื้อผ้าดี  ผ้าแข็งใหม่ สีเทาสนิท   ',
+    type: 'trousers'
+}, {
+    id: 12,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/372420749_177342988712343_5470321926260712608_n.jpg?stp=dst-jpg_s206x206&_nc_cat=104&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeGAwqxieMl0nofpu5k6vggkEp2YOAErMGISnZg4ASswYvn96CoYbhHWzb3kZ0NbtkjMc7-9wOhBpzQRBUne1wlz&_nc_ohc=sBhPeEuzwHQAX_272J8&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSCu87fwgtn1RPtL9K-6mFbgOkVJiIw7LP3ecJDLBqw7w&oe=651D4FAE',
+    name: 'Jack Purcell 1980 s (เขี้ยวขี้ม้า)',
+    price: 5500,
+    description: 'Size:4-4.5(23.5)    เหมาะกับเท้า37~38    Detail :แจ็คเขียวขี้ม้า80’s ซอฟเขียวตะเข็บจม แท้เดิมสภาพดีขอบยางดีเชือกไม่มีนะครับผม ซอฟในชัดเจนบีบกดได้ปกติ ป้ายหลังสึกจางตามอายุการใช้งาน  ',
     type: 'shoe'
-}];
+}, {
+    id: 13,
+    img: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/359438212_276182145058586_2764787103751781404_n.jpg?stp=dst-jpg_s206x206&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeHLBeoskWLNEWv5glKMvVtMj3CU23yEbEOPcJTbfIRsQyj8iVkgQggk3j1x3kiLZnx0j_ud6VIa7_7ABQQR49Vj&_nc_ohc=kVeorkikGlMAX9GNKsg&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSbodky5mTIUqnPhBxMcomOZ7iIYcpq4qx7w6OKhCNqtg&oe=651D5BA3',
+    name: 'Converse Jack Purcell made in USA',
+    price: 1500,
+    description: 'Navy Hunter 80s ตัวนี้เป็นตัวเก่าปลายปี80s    Size:7.5~8(26.5~27cm.)    Digital: ตัวนี้เป็นผ้ากํามะยียิ้มลั่นจัดๆ สภาพเกือบมือ1 ป้ายหลังเต็ม ซอฟสอาดชัดเจน หัวบีบกดได้ขอบยาง ไม่มีตําหนิ สะอาดกริบๆ เนื้อผ้าดีมาก 10/10มีป้ายแท็ก  ',
+    type: 'shoe'
+}, {
+    id: 14,
+    img: 'https://scontent.fkdt1-1.fna.fbcdn.net/v/t1.15752-9/361068050_663293478640437_2760638257933428524_n.jpg?stp=dst-jpg_s206x206&_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeFUWAKtPWWsEimg3k0l9CjMz7nV2FtW1cvPudXYW1bVyz0t42rXd42UPsDns09vcR0KRsGZaMk1QsGJKeNHiiat&_nc_ohc=fdj_Ilxvg4gAX8gL4CN&_nc_ht=scontent.fkdt1-1.fna&oh=03_AdS4D9DWj02PtUcj0BT1p1AYyuPH-MJ4QCh29aik4f5jXQ&oe=651D5A74',
+    name: 'Converse All star Desstock 80s',
+    price: 11500,
+    description: ' Color:Spearmint          Size:7.5Over8(26.5~27cm)    Detail:มือ1 พร้อมกล่องตรงรุ่น สภาพรวมๆ10/10สภาพDesstock เต็มทุกเต็มไม่เคยผ่านการใส่ กันชนสวยหัวเล็ก   ป้ายหลังเต็มๆ  ',
+    type: 'shoe'
+}, {
+    id: 15,
+    img: 'https://scontent.fkdt1-1.fna.fbcdn.net/v/t1.15752-9/372397990_170993222687472_209717129440250911_n.jpg?stp=dst-jpg_s206x206&_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeF_kkByGmszFJ_0Dkf8udMIYsv9FRLk1L5iy_0VEuTUvumGu9bKERRrVrtltNlJpYBHIHCYgXHcApc88YRt26cB&_nc_ohc=SztCmntlJ7MAX-WC0jb&_nc_ht=scontent.fkdt1-1.fna&oh=03_AdSQTZPzCvJBjyE3evW0mgvsk4pcBfhW5JMUgM0Jsqrafw&oe=651D4D59',
+    name: 'Jack Purcell Green 90s',
+    price: 18500,
+    description: 'Size8~8.5(27~27.5cm.)    Detail:เขียวตอง สีสด10/10 ป้ายหลสึกนิดหน่อย ตอก994 ซอฟในสะอาดชัดเจน ยิ้มลั่นผ้าใบไม่มีขาด พื้นเต็มๆ    หัวบีบกดได้ ปกติ ขอบยางไม่มีรอยแตก รวมๆ 10/10ครับ ',
+    type: 'shoe'
+}, {
+    id: 16,
+    img: 'https://scontent.fkdt1-1.fna.fbcdn.net/v/t1.15752-9/364260239_310051041470477_8056130934276688275_n.jpg?stp=dst-jpg_s206x206&_nc_cat=106&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeGiK0XWUgQ0wjN95vCnXwMXBzyazXfRMEoHPJrNd9EwSs02Raa-MGlDmaYYiTOE0pb7SKUrpEmtrMtNX9KYQ__0&_nc_ohc=vHBsD1v7vPgAX87XACY&_nc_ht=scontent.fkdt1-1.fna&oh=03_AdR4W_pT1cp2s8ssCRwWxzywqtml1LJ7HR49HYLcOFhc0w&oe=651D5D42',
+    name: 'Converse Jack Purcell 🇺🇸 3Black 1990’',
+    price: 18500,
+    description: 'Size : 7~7.5/26.5 Over 27         Details : สามดำแท้เดิม ตัวบาร์โค๊ดกลางปี 90sยิ้มสวยยิ้มลั่น ขอบยางแตกนิดหน่อยป้ายหลังเต็ม หัวมีลอยงา สีเข้ม10/10 ',
+    type: 'shoe' }  ];
 
 $(document).ready(() => {
     var html = '';
